caf28f1524940976567c25e6b8512aa05e0a1530
nelrafa103 <0d3ovk0ig@mozmail.com>
2022-05-06 07:35:09 -0400

    Testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginService } from './login/login.service';
-import { SignupService } from './signup/signup.service';
+import { LoginService } from './services/login/login.service';
+import { SignupService } from './services/signup/signup.service';
 
 @Module({
   imports: [],
